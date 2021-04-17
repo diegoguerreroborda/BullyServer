ae3cdbe402d759fd44fd9ff07d9d7128042dccac
Diego Guerrero <diego.guerrero02@uptc.edu.co>
2021-04-17 15:44:09 -0500

    ef

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let servers = [{path: "http://localhost:3000/", alive: false}, {path: "http://lo
 //let servers = []
 let isLeader = true;
 let leaderHost = 'http://localhost:3000/';
+let serversHiguer = [];
 
 //Envia latidos al lider cada 15 segundos
 //Los latidos se deben hacer en un timepo aleatorio.
@@ -26,14 +27,60 @@ var taskheartbeat = cron.schedule('*/25 * * * * *', () => {
             console.log(response.data)
         }).catch(function (error) {
             console.log("Escoger nuevo lider")
-            chooseNewLeader();
+            sendRequestToAll();
         });
     }
 });
 
-function chooseNewLeader() {
-    //get axios para ver cuales son mayores
+function sendRequestToAll(){
+    serversHiguer = [];
+    for (const server in servers) {
+        try {
+            response = await axios(`${servers[server].path}/id_server`)
+            servers[server].alive = true;
+            //Aqui busca solo los mayores que él.
+            if(response > id){
+                serversHiguer.push({path: servers[server].path, id = response});
+            }
+        } catch(err) {
+            //servers[server].alive = false;
+        }
+    }
+    chooseHiguer();
+    res.send(textServers);
 }
+
+function chooseHiguer(){
+    let numberHigh = 0;
+    for (const serverHigh in serversHiguer) {
+        if(serversHiguer[serverHigh].id > numberHigh){
+            numberHigh = servers[server].id
+        }
+    }
+    sendHeartBeatToLeader(numberHigh)
+}
+
+function sendHeartBeatToLeader(numberHigh){
+    for (const serverHigh in serversHiguer) {
+        if(serversHiguer[serverHigh].id == numberHigh){
+            axios.get(serversHiguer[serverHigh].path)
+            //Le dice que el va a ser el nuevo lider.
+            .then(function (response) {
+                console.log(response.data)
+            }).catch(function (error) {
+                console.log('Error ni el hp')
+            });
+        }
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendStatus(200)
+})
+
+app.get('/id_server', (req, res) => {
+    res.json(id)
+})
 
 //Para los latidos al lider
 app.get('/heartbeat_stop', (req, res) => {
