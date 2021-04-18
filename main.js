62e1018d9c907dc85323f8049e2322241caf9091
Diego Guerrero <diego.guerrero02@uptc.edu.co>
2021-04-17 19:18:17 -0500

    funciona escoger el lider

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const axios = require('axios');
 //const cors = require('cors');
 const shell = require('shelljs');
 var  cron  = require('node-cron');
-const { response } = require('express');
+const bodyParser = require('body-parser');
+//const { response } = require('express');
 
 const port = process.argv[2];
 
 const app = express()
+app.use(bodyParser.json())
 //app.use(cors())
 
 const id = port;
@@ -27,31 +29,56 @@ var taskheartbeat = cron.schedule('*/25 * * * * *', () => {
             //Hace una petición GET al servidor
             console.log(response.data)
         }).catch(function (error) {
+            //Parar todos los latidos
             console.log("Escoger nuevo lider")
-            sendRequestToAll();
+            getIds();
         });
     }
 });
 
-async function sendRequestToAll(){
-    console.log('Enviar peticiones a todos')
+async function getIds(){
     serversHiguer = [];
+    await getRequest('id_server')
+    chooseHiguer();
+}
+
+async function getRequest(urlFinal){
+    console.log('Enviar peticiones a todos')
+    //serversHiguer = [];
     for (const server in servers) {
         try {
-            response = await axios(`${servers[server].path}/id_server`)
-            console.log(response)
-            console.log('data', response.data)
-            servers[server].alive = true;
+            console.log(`${servers[server].path}urlFinal`)
+            response = await axios(`${servers[server].path}${urlFinal}`)
+            console.log(response.data)
+            //servers[server].alive = true;
             //Aqui busca solo los mayores que él.
-            if(response > id){
-                serversHiguer.push({path: servers[server].path, id : response});
+            if(response.data > id){
+                serversHiguer.push({path: servers[server].path, id : response.data});
             }
         } catch(err) {
+            console.log(err.Error)
             //servers[server].alive = false;
         }
     }
-    chooseHiguer();
-    //res.send(textServers);
+}
+
+async function sendLeader(afterUrl){
+    console.log("Enviando lider a todos...")
+    for (const host in servers) {
+        console.log(servers[host].path)
+        await axios({
+            method: 'post',
+            url : `${servers[host].path}${afterUrl}`,
+            data: {
+              leader: leaderHost
+            }
+        }).then(response => {
+            console.log('Resultado:', response.data);
+        }).catch(err => {
+            console.log("Apagadooo")
+            //servers[host].alive = false;
+        });
+    }
 }
 
 function chooseHiguer(){
@@ -62,7 +89,7 @@ function chooseHiguer(){
     }else{
         for (const serverHigh in serversHiguer) {
             if(serversHiguer[serverHigh].id > numberHigh){
-                numberHigh = servers[server].id
+                numberHigh = serversHiguer[serverHigh].id
             }
         }
         sendHeartBeatToLeader(numberHigh)
@@ -76,6 +103,7 @@ async function sendHeartBeatToLeader(numberHigh){
             console.log(serversHiguer[serverHigh].path)
             axios.get(serversHiguer[serverHigh].path)
             //Le dice que el va a ser el nuevo lider.
+            //Comenzar otra vez con los latidos
             .then(function (response) {
                 console.log(response.data)
             }).catch(function (error) {
@@ -83,7 +111,15 @@ async function sendHeartBeatToLeader(numberHigh){
             });
         }
     }
+    //Otra vez envia el mensaje a todos con el nuevo lider.
+    sendLeader('new_leader')
 }
+
+app.post('/new_leader', (req, res) => {
+    leaderHost = req.body.leader;
+    console.log(leaderHost)
+    res.sendStatus(200)
+})
 
 app.get('/', (req, res) => {
     res.sendStatus(200)
